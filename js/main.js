c79d0d874d6adeead37c8d238ad2682f29962b5d
unknown <alhubu26@gmail.com>
2025-04-29 18:23:57 +0300

    Initial commit for nova-digital project

---
+++ js/main.js
@@ -0,0 +1,66 @@
+// Countdown Timer
+// 1000 milliseconds = 1 second
+
+let countDownDate = new Date("Dec 31, 2025 23:59:59").getTime(); // Set the countdown target date
+
+let counter = setInterval(() => {
+  // Get the current date and time
+  let dateNow = new Date().getTime();
+
+  // Calculate the difference between the target date and the current date
+  let dateDiff = countDownDate - dateNow;
+
+  // Calculate time units (days, hours, minutes, seconds)
+  let days = Math.floor(dateDiff / (1000 * 60 * 60 * 24));
+  let hours = Math.floor((dateDiff % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  let minutes = Math.floor((dateDiff % (1000 * 60 * 60)) / (1000 * 60));
+  let seconds = Math.floor((dateDiff % (1000 * 60)) / 1000);
+
+  // Update the HTML elements with the calculated values
+  document.querySelector(".days").innerHTML = days < 10 ? `0${days}` : days;
+  document.querySelector(".hours").innerHTML = hours < 10 ? `0${hours}` : hours;
+  document.querySelector(".minutes").innerHTML = minutes < 10 ? `0${minutes}` : minutes;
+  document.querySelector(".seconds").innerHTML = seconds < 10 ? `0${seconds}` : seconds;
+
+  // Stop the timer if the countdown is over
+  if (dateDiff < 0) {
+    clearInterval(counter);
+  }
+}, 1000);
+
+/* Animate Width On Scrolling */
+
+/* Increase Numbers On Scrolling */
+
+let progressSpans = document.querySelectorAll(".the-progress span");
+let section = document.querySelector(".our-skills");
+
+let nums = document.querySelectorAll(".stats .number");
+let statsSection = document.querySelector(".stats");
+let started = false; // Function Started ? No
+
+window.onscroll = function () {
+  // Skills Animate Width
+  if (window.scrollY >= section.offsetTop - 250) {
+    progressSpans.forEach((span) => {
+      span.style.width = span.dataset.width;
+    });
+  }
+  // Stats Increase Number
+  if (window.scrollY >= statsSection.offsetTop) {
+    if (!started) {
+      nums.forEach((num) => startCount(num));
+    }
+    started = true;
+  }
+};
+
+function startCount(el) {
+  let goal = el.dataset.goal;
+  let count = setInterval(() => {
+    el.textContent++;
+    if (el.textContent == goal) {
+      clearInterval(count);
+    }
+  }, 2000 / goal);
+}
